c87e8acc624e03963f37ec9fb8bd7d850b86b816
jessicagold@mail.tau.ac.il <jessicagold@mail.tau.ac.il>
2020-11-19 16:42:53 +0200

    commit befor branch

--- src/ast/MethodRenameVisitor.java
+++ src/ast/MethodRenameVisitor.java
@@ -104,6 +104,7 @@ public class MethodRenameVisitor implements Visitor {
 		for(Statement statement : methodDecl.body()) {
 			statement.accept(this);
 		}
+		//methodDecl.ret().accept(this); casting?
 
 	}
 
