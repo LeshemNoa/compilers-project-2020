17db8e599aca5de2af4b4c70e37e6c4116e90287
jessicagold@mail.tau.ac.il <jessicagold@mail.tau.ac.il>
2020-12-02 21:57:38 +0200

    continued some implementation of the visitor, mostly helper function signatures and simple loops

--- src/ast/LlvmVisitor.java
+++ src/ast/LlvmVisitor.java
@@ -20,28 +20,63 @@ public class LlvmVisitor implements Visitor{
             "    call i32 (i8*, ...) @printf(i8* %_str)\n" +
             "    call void @exit(i32 1)\n" +
             "    ret void\n" +
-            "}";
+            "}\n";
 
     private String LlvmProgram;
     private InheritanceForest forest;
     private SymbolTable programSymbolTable;
     private Map<String, List<STSymbol>> vtables;
+    private int registerPerMethodCounter;
+    private Map<String, Integer> alocationSizeForClassInstance;
 
     public LlvmVisitor(Program program){
         LlvmProgram = "";
         forest = new InheritanceForest(program);
         programSymbolTable = new SymbolTable(program);
         vtables = STLookup.getClassesAndMethodsForVtable(programSymbolTable, forest);
+
+        alocationSizeForClassInstance = new HashMap<>();
+        for(ClassDecl classDecl : program.classDecls()){
+            alocationSizeForClassInstance.put(classDecl.name(), getAlocationSize(classDecl));
+        }
+    }
+
+    private int getAlocationSize(ClassDecl classDecl){
+        //TODO
+        //this needs to be changed to a recursive function on trees because of inherited fields!!
+        /*find out how many int field, IntArray fields and their lengths, boolean fields and reference fields there are
+        * don't forget to add 8 for vtable pointer*/
     }
 
     @Override
     public void visit(Program program) {
+        LlvmProgram = HELPER_METHODS + "\n";
+        for(ClassDecl classDecl : forest.getRoots()){
+            recursiveVisitTree(classDecl);
+        }
+        program.mainClass().accept(this);
+    }
+
+    private void recursiveVisitTree(ClassDecl classDecl){
+        classDecl.accept(this);
+        if(forest.getChildren(classDecl) == null) return;;
+        for(ClassDecl child : forest.getChildren(classDecl)){
+            recursiveVisitTree(child);
+        }
+    }
+
+    private String generateVtable(ClassDecl classDecl){
+        List<STSymbol> methods = this.vtables.get(classDecl.name());
+        //TODO
 
     }
 
     @Override
     public void visit(ClassDecl classDecl) {
-
+        LlvmProgram = LlvmProgram.concat(generateVtable(classDecl));
+        for(MethodDecl methodDecl : classDecl.methoddecls()){
+            methodDecl.accept(this);
+        }
     }
 
     @Override
@@ -51,7 +86,7 @@ public class LlvmVisitor implements Visitor{
 
     @Override
     public void visit(MethodDecl methodDecl) {
-
+        registerPerMethodCounter = 0;
     }
 
     @Override
