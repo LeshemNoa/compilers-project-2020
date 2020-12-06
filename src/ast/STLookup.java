1cd2a9f891b49c1e871303e1ae778620b13c64f6
Noa Leshem <>
2020-12-06 13:20:40 +0200

    Edit existing STLookup methods to create two maps - for instance shapes and for VTables

--- src/ast/STLookup.java
+++ src/ast/STLookup.java
@@ -33,26 +33,35 @@ public class STLookup {
 
 
     /**
-     *
-     * @param prog
-     * @param forest
-     * @return
+     * Creates two mappings:
+     * 1. Map from class names to lists of method declarations that belong to them,
+     *      either by declaration or by inheritance.
+     * 2. Map from class names to lists of field declarations that belong to them,
+     *      either by declaration or by inheritance.
+     * @param prog      Symbol table for entire program
+     * @param forest    Class inheritance forest
+     * @return          List containing the two maps
      */
-    public static Map<String, List<STSymbol>> getClassesAndMethodsForVtable(SymbolTable prog, InheritanceForest forest){
-        Map<String, List<STSymbol>> res = new HashMap<>();
+    public static List<Map<String, List<STSymbol>>> createProgramMaps(SymbolTable prog, InheritanceForest forest){
+        Map<String, List<STSymbol>> vtablesMap = new HashMap<>();
+        Map<String, List<STSymbol>> instanceTemplateMap = new HashMap<>();
 
         for(ClassDecl classDecl : forest.getRoots()){
-            RecursiveMethodSymbolsToRes(classDecl, prog, forest, res);
+            recPopulateMaps(classDecl, prog, forest, vtablesMap, instanceTemplateMap);
         }
-
-        return res;
+        
+        List<Map<String, List<STSymbol>>> results = new ArrayList<>();
+        results.add(vtablesMap);
+        results.add(instanceTemplateMap);
+        return results;
     }
 
+
     /**
-     *
-     * @param classDecl
-     * @param classTable
-     * @return
+     * Returns list of methods declared in provided class.
+     * @param classDecl     declaration node for said class
+     * @param classTable    The class's symbol table
+     * @return              List of methoddecls
      */
     private static List<STSymbol> getMethodSymbols(ClassDecl classDecl, SymbolTable classTable){
         List<STSymbol> res = new ArrayList<>();
@@ -65,11 +74,27 @@ public class STLookup {
     }
 
     /**
-     *
-     * @param classDecl
-     * @param classTable
-     * @param parentMethods
-     * @return
+     * Returns list of fields declared in provided class.
+     * @param classDecl     declaration node for said class
+     * @param classTable    The class's symbol table
+     * @return              List of vardecls
+     */
+    private static List<STSymbol> getFieldSymbols(ClassDecl classDecl, SymbolTable classTable){
+        List<STSymbol> res = new ArrayList<>();
+
+        for(VarDecl field: classDecl.fields()){
+            res.add(classTable.getSymbol(field.name(), false));
+        }
+
+        return res;
+    }
+
+    /**
+     * Returns list of methods, both declared and inherited, in provided class.
+     * @param classDecl      declaration node for said class
+     * @param classTable     The class's symbol table
+     * @param parentMethods  List of methods inherited from the superclass
+     * @return List of methoddecls
      */
     private static List<STSymbol> getMethodSymbols(ClassDecl classDecl, SymbolTable classTable, List<STSymbol> parentMethods){
         Set<String> localDeclNames = new HashSet<>();
@@ -82,7 +107,7 @@ public class STLookup {
 
         List<STSymbol> res = new ArrayList<>();
         for(STSymbol symbol : parentMethods){
-            if(!localDeclNames.contains(symbol.name())) res.add(symbol);
+            if (!localDeclNames.contains(symbol.name())) res.add(symbol);
         }
         res.addAll(localDecls);
 
@@ -90,25 +115,62 @@ public class STLookup {
     }
 
     /**
-     *
-     * @param classDecl
-     * @param forest
-     * @param res
+     * Returns list of fields, both declared and inherited, in provided class.
+     * @param classDecl      declaration node for said class
+     * @param classTable     The class's symbol table
+     * @param parentFields  List of fields inherited from the superclasses
+     * @return List of vardecls
      */
-    private static void RecursiveMethodSymbolsToRes(ClassDecl classDecl, SymbolTable prog, InheritanceForest forest, Map<String, List<STSymbol>> res){
+    private static List<STSymbol> getFieldSymbols(ClassDecl classDecl, SymbolTable classTable, List<STSymbol> parentFields){
+        Set<String> localDeclNames = new HashSet<>();
+        List<STSymbol> localDecls = new ArrayList<>();
+
+        for(VarDecl field : classDecl.fields()){
+            localDecls.add(classTable.getSymbol(field.name(), false));
+            localDeclNames.add(field.name());
+        }
+
+        List<STSymbol> res = new ArrayList<>();
+        for(STSymbol symbol : parentFields){
+            if (!localDeclNames.contains(symbol.name())) res.add(symbol);
+        }
+        res.addAll(localDecls);
+
+        return res;
+    }
+
+    /**
+     * Recursive function traverse the inheritance forest and populate the VT map and the instance map.
+     * @param classDecl     Current class declaration reached during traversal, class whose method list is created
+     * @param forest        Inheritance forest for class hierarchy, for incremental construction of each class's
+     *                      method list, implementing prefixing
+     * @param VTMap         VT hash map into which the collected method list is added
+     * @param instanceMap   VT hash map into which the collected field list is added
+     */
+    private static void recPopulateMaps(
+            ClassDecl classDecl,
+            SymbolTable prog,
+            InheritanceForest forest,
+            Map<String, List<STSymbol>> VTMap,
+            Map<String, List<STSymbol>> instanceMap
+    ){
         SymbolTable classTable = prog.getSymbol(classDecl.name(), false).enclosedScope();
 
-        if(classDecl.superName() == null && !res.containsKey(classDecl.name())){
-            res.put(classDecl.name(), getMethodSymbols(classDecl, classTable));
+        if (classDecl.superName() == null && !VTMap.containsKey(classDecl.name())){
+            VTMap.put(classDecl.name(), getMethodSymbols(classDecl, classTable));
+            instanceMap.put(classDecl.name(), getFieldSymbols(classDecl, classTable));
         }
-        else{//at this point we assume that the super list of STSymbols is already in res
-            res.put(classDecl.name(), getMethodSymbols(classDecl, classTable, res.get(classDecl.superName())));
+        else { //at this point we assume that the super list of STSymbols is already in res
+            VTMap.put(classDecl.name(), getMethodSymbols(classDecl, classTable, VTMap.get(classDecl.superName())));
+            instanceMap.put(classDecl.name(), getFieldSymbols(classDecl, classTable, instanceMap.get(classDecl.superName())));
         }
 
-        if(forest.getChildren(classDecl) == null) return;
+        if (forest.getChildren(classDecl) == null) return;
 
-        for(ClassDecl child : forest.getChildren(classDecl)){
-            RecursiveMethodSymbolsToRes(child, prog, forest, res);
+        for (ClassDecl child : forest.getChildren(classDecl)) {
+            recPopulateMaps(child, prog, forest, VTMap, instanceMap);
         }
     }
+
+
 }
